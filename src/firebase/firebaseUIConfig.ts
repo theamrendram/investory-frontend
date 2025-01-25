2ffe190dad16b3a5e68e7141eacf9756f685ac76
KumarAmrendram <amrendramkumar@gmail.com>
2025-01-25 11:27:38 +0530

    build fix: removed firebase ui library

--- src/firebase/firebaseUIConfig.ts
+++
@@ -1,9 +0,0 @@
-import { GoogleAuthProvider } from "firebase/auth";
-
-export const firebaseUiConfig = {
-  signInFlow: "popup",
-  signInOptions: [GoogleAuthProvider.PROVIDER_ID],
-  callbacks: {
-    signInSuccessWithAuthResult: () => false, // Avoid redirect after sign-in
-  },
-};
